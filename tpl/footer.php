2d743bac9a1d60697248fc12bea66eafc9529093
Mattias Nyberg <muthaias.uu@gmail.com>
2014-10-26 01:24:56 +0200

    * Added beer license text to templates.

--- tpl/footer.php
+++ tpl/footer.php
@@ -1,3 +1,13 @@
+<?php
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <me@cp0la.se> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Mattias Nyberg
+ * ----------------------------------------------------------------------------
+ */
+?>
 		<hr/>
     </div><!-- /.container -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
--- tpl/header.php
+++ tpl/header.php
@@ -1,4 +1,14 @@
 <!DOCTYPE html>
+<?php
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <me@cp0la.se> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Mattias Nyberg
+ * ----------------------------------------------------------------------------
+ */
+?>
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
